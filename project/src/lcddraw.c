1b0f2832d698cad25d1fb32eb9d5614266347b2d
millieb <mildred.brito@outlook.com>
2020-07-27 14:54:16 -0600

    added font 8x12

--- project/src/lcddraw.c
+++ project/src/lcddraw.c
@@ -37,6 +37,29 @@ void fillRectangle(u_char colMin, u_char rowMin, u_char width, u_char height,
   }
 }
 
+/** Draw and fill Iso Triangle
+ *
+ *  \param colMin Column start
+ *  \param rowMin Row start
+ *  \param width Width of triangle
+ *  \param height height of triangle
+ *  \param colorBGR Color of triangle in BGR
+ **/
+
+void drawIsoTriangle(u_char colMin, u_char rowMin, double width, double height, u_int colorBGR) {
+  u_char i;
+  double slope = width/height;
+  double middle = width/2;
+
+  u_char j;
+
+  for(i = 0; i <= height; i++) {
+    for(j = colMin + middle - slope/2 * i; j <= colMin + middle + slope/2 * i; j++) {
+      drawPixel(j, i + rowMin, colorBGR);
+    }
+  }
+}
+
 /** Clear screen (fill with color)
  *  
  *  \param colorBGR The color to fill screen
@@ -91,6 +114,40 @@ void drawString5x7(u_char col, u_char row, char *string,
   while (*string) {
     drawChar5x7(cols, row, *string++, fgColorBGR, bgColorBGR);
     cols += 6;
+  }
+}
+
+/* 8x12 font - this function draws background pixels
+ * adapted from RobG's EduKit
+ */
+
+void drawChar8x12(u_char rcol, u_char rrow, char c, u_int fgColorBGR, u_int bgColorBGR)
+{
+  u_char col = 0;
+  u_int row = 0;
+  u_char bit = 0x80;
+  u_char oc = c - 0x20;
+
+  lcd_setArea(rcol, rrow, rcol + 7, rrow + 12);
+  while(row < 12){
+    while(col < 8){
+      u_int colorBGR = (font_8x12[oc][row] & bit) ? fgColorBGR : bgColorBGR;
+      lcd_writeColor(colorBGR);
+      col++;
+      bit >>= 1;
+    }
+    col = 0;
+    bit = 0x80;
+    row++;
+  }
+}
+
+void drawString8x12(u_char col, u_char row, char *string, u_int fgColorBGR, u_int bgColorBGR)
+{
+  u_char cols = col;
+  while (*string) {
+    drawChar8x12(cols, row, *string++, fgColorBGR, bgColorBGR);
+    cols += 9;
   }
 }
 
