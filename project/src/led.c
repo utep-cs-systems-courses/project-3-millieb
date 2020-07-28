32dbb874169e06c459f00fb3f71b4054f742ee5d
millieb <mildred.brito@outlook.com>
2020-07-27 18:16:17 -0600

    updated led_update, led_toggle, and led_dim()

---
+++ project/src/led.c
@@ -0,0 +1,81 @@
+#include <msp430.h>
+#include "led.h"
+#include "switches.h"
+
+
+void led_init()
+{
+  P1DIR |= LEDS;		// bits attached to leds are output
+  leds_changed = 1;
+}
+
+void led_update()
+{
+  if (leds_changed)
+    {
+      char ledFlags = 0; //starting with leds off
+
+      ledFlags |= leds_changed ? LED_GREEN : 0;
+      ledFlags |= leds_changed ? 0 : LED_RED;
+
+      P1OUT &= (0xff - LEDS) | ledFlags; // clear bit for off leds
+      P1OUT |= ledFlags;                // set bit for on leds
+    }
+  leds_changed = 0;
+}
+
+void toggle_on()
+{
+  P1OUT = LED_RED;
+  __delay_cycles(500000);
+  P1OUT = LED_GREEN;
+  __delay_cycles(500000);
+}
+
+void toggle_off()
+{
+  P1OUT = !(LED_GREEN);
+  __delay_cycles(500000);
+  P1OUT = !LED_RED;
+  __delay_cycles(500000);
+}
+
+void led_dim()
+{
+  for(int i = 0; i < 1000; i++)
+    {
+      P1OUT = LED_RED;
+      __delay_cycles(10000);
+      P1OUT = LED_GREEN;
+      __delay_cycles(10000);
+      P1OUT = !LED_RED;
+      __delay_cycles(10000);
+      P1OUT = !LED_GREEN;
+      __delay_cycles(10000);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
